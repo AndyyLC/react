24f65c436bef5c7a4278946181a3987728631670
Andyy <andy12312345@hotmail.com>
2023-06-22 18:13:20 -0400

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+
+function ListGroup() {
+
+    const items = [
+        'Cheese',
+        'Quasso',
+        'meat',
+        'ello'
+    ]
+
+    if (items.length === 0)
+        return <p>No item found</p>
+    return (
+    <Fragment>
+        <h1>List</h1>   
+        <ul className="list-group">
+            {items.map(item => 
+                <li key={item}>{item}</li>)}
+        </ul>
+    </Fragment>
+    );
+}
+
+export default ListGroup;
