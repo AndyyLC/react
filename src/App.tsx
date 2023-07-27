44ab4462cae18698ab3fcd68d839d327b7743ec8
Andyy <andy12312345@hotmail.com>
2023-07-27 16:55:58 -0400

    Added button functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Alert from "./components/Alert";
+import { Button } from "./components/Button";
 
 function App() {
 
-  return <div>
+  return (<div>
     <Alert>
       You have won an Iphone
     </Alert>
+
+    <Button onClick={() => console.log('Clicked')}>Button</Button>
     </div>
+  );
 }
 export default App;
